eee7d042e8eae7341969207959995a07180c11f7
Dan Groleau <Daniel.Groleau@tivityhealth.com>
2021-05-22 17:45:46 -0600

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ASPDotNetRazorApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string PartyName { get; set; }
+        public int PartySize { get; set; }
+        public string PartyPreference { get; set; }
+        public List<SelectListItem> PartyPreferences { get; set; }
+        public string PartyPhoneNumber { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
